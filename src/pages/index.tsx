7c65042f0a8c0fa8e037981e4d39d34a78202816
andre-silva-14 <silva.andre1997@gmail.com>
2021-02-26 17:46:17 +0100

    Add CountDown Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import Head from "next/head";
 
 import styles from '../styles/pages/Home.module.css'
 
@@ -8,11 +10,17 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Flex.it</title>
+      </Head>
+
       <ExperienceBar/>
+
       <section className={styles.leftContainer}>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
       </section>
     </div>
